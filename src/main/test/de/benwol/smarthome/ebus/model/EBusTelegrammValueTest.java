5a0c7988238a74559d6502dacf67d7aa4eb5b02a
Ben Wolters <18100784+woldinius@users.noreply.github.com>
2018-07-31 23:38:18 +0200

    fix2: started creating tests; under construction

--- src/main/test/de/benwol/smarthome/ebus/model/EBusTelegrammValueTest.java
+++ src/main/test/de/benwol/smarthome/ebus/model/EBusTelegrammValueTest.java
@@ -1,20 +1,24 @@
 package de.benwol.smarthome.ebus.model;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import org.junit.Test;
 
-@Data
-@EqualsAndHashCode
-public class EBusTelegrammValue {
-    private byte[] value;
-    private int index;
-    private int length;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Is.is;
+import static org.hamcrest.Null.nonNull;
 
-    public EBusTelegrammValue(byte[] value, int index, int length) {
-        assert (value.length == length);
-        assert (index > 0);
-        setValue(value);
-        setIndex(index);
-        setLength(length);
+public class EBusTelegrammValueTest {
+
+    private final static byte[] TEST_VALUE = new byte[] { 0x01, 0x01 };
+
+    @Test
+    public void assertionTestEBusTelegrammValueIndex() {
+        EBusTelegrammValue eBusTelegrammValue = new EBusTelegrammValueTest(TEST_VALUE, -1, TEST_VALUE.length);
+        assertThat("valid EBusTelegramm expected", eBusTelegrammValue, is(nonNull()));
+
+        try {
+            new EBusTelegrammValueTest(TEST_VALUE, -1, TEST_VALUE.length);
+        } catch (AssertionError assertionError) {
+            assertThat("assertion error expected", assertionError, is(nonNull()));
+        }
     }
 }
